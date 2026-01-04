f3c8820ab077886d7cb6d28f4d512c3fe9b34859
Ton Tan Khang <87051539+KhangToon@users.noreply.github.com>
2026-01-04 10:22:12 +0700

    Add deploy doc

--- todo-app/vite.config.ts
+++ todo-app/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-todo-app/', // ⚠️ Thay 'todo-list' bằng tên repo của bạn
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
